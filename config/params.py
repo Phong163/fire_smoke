3850d34bb4904ef4d9323acb8226a2e87d66a325
Hoàng Văn Thái <135320650+Phong163@users.noreply.github.com>
2026-01-30 15:57:18 +0700

    Update params.py

--- config/params.py
+++ config/params.py
@@ -3,7 +3,12 @@ camera_configs = {
         "box_id":"d43050fd-7296-4409-ad54-757af392a0d2",
         "cameras":{
             0: {
-                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/1001",
+                "rtsp_url": "rtsp://admin:eidox2026@dvhcam1.cameraddns.net:5554/live",
+                "track_person": {},
+                "current_frame": 0
+            },
+            1: {
+                "rtsp_url": "rtsp://admin:eidox2026@dvhcam2.cameraddns.net:5555/live",
                 "track_person": {},
                 "current_frame": 0
             },
